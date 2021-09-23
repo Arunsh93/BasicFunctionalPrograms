5cd2278395642624cae1cefae26e4eee2eba2a47
Arunsh93 <Arunhosur93@gmail.com>
2021-09-23 14:31:32 +0530

    Head and Tail Percentage

---
+++ HeadAndTailPercentage/UC1_HeadAndTail.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace HeadAndTailPercentage
+{
+    class UC1_HeadAndTail
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the Number of times to flipcoin");
+            int NumberOfFlips = int.Parse(Console.ReadLine());
+
+            int HeadCount = 0;
+            int TailCount = 0;
+            int HeadPercentage = 0;
+            int TailPercentage = 0;
+
+            for (int i = 0; i < NumberOfFlips; i++)
+            {
+                Random random = new Random();
+                int randomNumber = random.Next(2);
+
+                if (randomNumber == 1)
+                {
+                    HeadCount++;
+                }
+                else if (randomNumber == 0)
+                {
+                    TailCount++;
+                }
+
+                Console.WriteLine("Head Counts: " + HeadCount);
+                Console.WriteLine("Tail Counts: " + TailCount);
+                Console.WriteLine("Flips : " + NumberOfFlips);
+
+                HeadPercentage = ((HeadCount * 100) / NumberOfFlips);
+                TailPercentage = ((TailCount * 100) / NumberOfFlips);
+
+                Console.WriteLine("Head Percentage is : " + HeadPercentage);
+                Console.WriteLine("Tail Percentage is : " + TailPercentage);
+            }
+        }
+    }
+}
