e417a01a68a46bc88d7cf2c56f5eadd0100731c6
Arunsh93 <Arunhosur93@gmail.com>
2021-09-23 16:33:37 +0530

    Calculate Harmonic Number

---
+++ HarmonicNumber/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace HarmonicNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double result = 0.0;
+            Console.WriteLine("Enter the Value For Harmonic");
+            int number = int.Parse(Console.ReadLine());
+            for(int i =0; i<=number; i++)
+            {
+                result += 1 / (float)i;
+                Console.WriteLine("1/{0} + ", i);
+            }
+
+            Console.WriteLine($"={result}");
+        }
+    }
+}
