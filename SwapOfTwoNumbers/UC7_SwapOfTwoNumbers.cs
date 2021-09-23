692be6432d48925dc44a60130217116902aa7e74
Arunsh93 <Arunhosur93@gmail.com>
2021-09-23 18:01:14 +0530

    Swap of Two Numbers

---
+++ SwapOfTwoNumbers/UC7_SwapOfTwoNumbers.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace SwapOfTwoNumbers
+{
+    class UC7_SwapOfTwoNumbers
+    {
+        static void Main(string[] args)
+        {
+            int FirstNumber = 0;
+            int SecondNumber = 0;
+            int temp = 0;
+            Console.Write("Enter First Number: ");
+            FirstNumber = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter Second Number: ");
+            SecondNumber = int.Parse(Console.ReadLine());
+
+            temp = FirstNumber;
+            FirstNumber = SecondNumber;
+            SecondNumber = temp;
+
+            Console.WriteLine("\nAfter Swapping : ");
+            Console.WriteLine("\nFirst Number : " + FirstNumber);
+            Console.WriteLine("Second Number : " + SecondNumber);
+        }
+    }
+}
