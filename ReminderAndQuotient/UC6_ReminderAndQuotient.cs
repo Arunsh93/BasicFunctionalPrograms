9144260674eb96be283e039dec9e7cb92fa3e537
Arunsh93 <Arunhosur93@gmail.com>
2021-09-23 17:37:14 +0530

    Reminder And Quotient Problem

---
+++ ReminderAndQuotient/UC6_ReminderAndQuotient.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ReminderAndQuotient
+{
+    class UC6_ReminderAndQuotient
+    {
+        static void Main(string[] args)
+        {
+            int Dividend = 0;
+            int Divisor = 0;
+            
+            Console.Write("Enter the Dividend: ");
+            Dividend = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter the Divisor: ");
+            Divisor = int.Parse(Console.ReadLine());
+
+            int Quotient = Dividend / Divisor;
+            int Reminder = Dividend % Divisor;
+
+            Console.WriteLine("Dividend:{0} Divisor{1}", Dividend, Divisor);
+            Console.WriteLine("Quotient: " + Quotient);
+            Console.WriteLine("Reminder: " + Reminder);
+        }
+    }
+}
