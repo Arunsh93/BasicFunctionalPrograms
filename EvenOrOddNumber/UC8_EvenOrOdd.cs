c52b3e3b8127d9b6b40117cb6be4953c83e646a6
Arunsh93 <Arunhosur93@gmail.com>
2021-09-23 17:16:34 +0530

    Even or Odd number Program

---
+++ EvenOrOddNumber/UC8_EvenOrOdd.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace EvenOrOddNumber
+{
+    class UC8_EvenOrOdd
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the Number to check Even Or Odd");
+            int number = int.Parse(Console.ReadLine());
+
+            if (number % 2 == 0)
+            {
+                Console.WriteLine(number + "The Number Even");
+            }
+            else
+            {
+                Console.WriteLine(number + "The number is Odd");
+            }
+        }
+    }
+}
