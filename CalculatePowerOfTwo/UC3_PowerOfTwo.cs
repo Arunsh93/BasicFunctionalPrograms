b7cdb9bb9dd0d47ff6549f8ab55eb8ae8810a14a
Arunsh93 <Arunhosur93@gmail.com>
2021-09-23 15:23:52 +0530

    Calculate Power of Two

---
+++ CalculatePowerOfTwo/UC3_PowerOfTwo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace CalculatePowerOfTwo
+{
+    class UC3_PowerOfTwo
+    {
+        static void Main(string[] args)
+        {
+            double power = 0;
+            Console.WriteLine("Enter the Number to calculate Power of Two");
+            int number = int.Parse(Console.ReadLine());
+            
+            for(int i = 0; i <= number; i++)
+            {
+                if(number<31)
+                {
+                    power = Math.Pow(2, number);
+                }
+                else
+                {
+                    Console.WriteLine("Over Flow");
+                }
+            }
+
+            Console.WriteLine(power);
+        }
+    }
+}
